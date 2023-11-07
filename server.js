b0717e36630eb60f078152ef7f97f7c1fdb9ea3f
R S <manik9092526954@gmail.com>
2023-11-07 17:11:42 +0530

    backend

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutsRouter = require("./routes/workouts");
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.use("/api/workouts", workoutsRouter);
+
+mongoose
+  .connect(process.env.MONG_URL)
+  .then(() => {g
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
