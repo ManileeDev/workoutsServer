5cca0069def33c2738c77b344eb6d432ea73477f
R S <manik9092526954@gmail.com>
2023-11-08 16:40:56 +0530

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const workoutsRouter = require("./routes/workouts");
 const PORT = process.env.PORT || 3500;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
