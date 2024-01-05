5aa94fd434a8addc09aadf7d520d3f664388d793
R S <manik9092526954@gmail.com>
2024-01-05 18:10:12 +0530

    new middleware added

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ const PORT = process.env.PORT || 3500;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use("/api/workouts", workoutsRouter);
 app.use("/api/users",usersRouter)
 mongoose
